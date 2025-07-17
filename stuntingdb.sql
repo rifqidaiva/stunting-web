440aaf48fe3bd550582e9812dcd3fbd991b6f7a7
rifqidaiva <rivkidaiva123@gmail.com>
2025-07-17 15:31:41 +0700

    Rename stunting_geojson.sql to stuntingdb.sql
    Renamed the SQL file to better reflect its purpose. Updated the generation timestamp in the file header.

--- stuntingdb.sql
+++ stuntingdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2025 at 07:45 AM
+-- Generation Time: Jul 17, 2025 at 10:30 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
