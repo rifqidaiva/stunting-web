05112a7a8800a32ecdc2bde094cc62d3340a294c
rifqidaiva <rivkidaiva123@gmail.com>
2025-07-22 07:55:52 +0700

    Update GeoJSON loading and boundary metadata
    Refactored leaflet.js to fetch GeoJSON data from a new API endpoint and support multiple layers with a layer control. Removed unused 'name' and 'crs' metadata from cirebon_boundary.geojson for cleaner structure.

--- assets/leaflet.js
+++ assets/leaflet.js
@@ -9,19 +9,33 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 async function getStuntingData() {
   try {
-    const response = await fetch("static/geojson.geojson")
-    const stuntingGeoJson = await response.json()
+    const response = await fetch("/api/geojson/get")
+    const responseJson = await response.json()
+    const stuntingGeoJsonArray = responseJson.data
 
-    L.geoJSON(stuntingGeoJson, {
-      onEachFeature: function (feature, layer) {
-        let props = feature.properties
-        let tableRows = Object.entries(props)
-          .map(([key, value]) => `<tr><th>${key}</th><td>${value}</td></tr>`)
-          .join("")
-        let popupContent = `<table class="table" border="1">${tableRows}</table>`
-        layer.bindPopup(popupContent)
-      },
-    }).addTo(map)
+    const layers = {}
+
+    stuntingGeoJsonArray.forEach((geojson, idx) => {
+      const layer = L.geoJSON(geojson, {
+        onEachFeature: function (feature, layer) {
+          let props = feature.properties
+          let tableRows = Object.entries(props)
+            .map(([key, value]) => `<tr><th>${key}</th><td>${value}</td></tr>`)
+            .join("")
+          let popupContent = `<table class="table" border="1">${tableRows}</table>`
+          layer.bindPopup(popupContent)
+        },
+      })
+
+      const layerName = geojson.name || `Layer ${idx + 1}`
+      layers[layerName] = layer
+    })
+
+    L.control.layers(null, layers, { collapsed: false }).addTo(map)
+
+    // Add the first layer to the map by default
+    const firstLayer = Object.values(layers)[0]
+    if (firstLayer) firstLayer.addTo(map)
   } catch (error) {
     console.error("Error loading geojson:", error)
   }
